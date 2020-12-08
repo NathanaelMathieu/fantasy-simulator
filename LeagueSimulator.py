280cac95f017147e845ee1d598b9c633fa09c703
Nathanael Mathieu <nmathieu@wesleyan.edu>
2020-12-07 21:35:25 -0500

    Track wins/losses/ties seperately

--- LeagueSimulator.py
+++ LeagueSimulator.py
@@ -21,30 +21,18 @@ class FantasyTeam(object):
         self.name=name
         self.pf=pf
         self.pa=pa
-        if len(record) == 3:
-            self.record = record
-        else:
-            self.record=[record[0], record[1], 0]
+        self.wins=record[0]
+        self.losses=record[1]
+        self.ties=record[2]
     
     def __str__(self):
         return self.name
-    
-    #get functions
-    def getWins(self):
-        return self.record[0]
-    def getLosses(self):
-        return self.record[1]
-    def getTies(self):
-        return self.record[2]
 
     #set functions
     def setRecord(self, record):
-        for i in range(0, min(3, len(record))):
-            self.record[i] = record[i]
-    def addWin(self):
-        self.record[0]+=1
-    def addLoss(self):
-        self.record[1]+=1
+        self.wins=record[0]
+        self.losses=record[1]
+        self.ties=record[2]
 
 def createTeams(league):#Current Teams and records
     teams = {}
@@ -56,48 +44,59 @@ def createTeams(league):#Current Teams and records
 
 
 def chooseWinner(home_team, away_team, override = False): #
-    home_ppg = home_team.pf / sum(home_team.record)
-    away_ppg = away_team.pf / sum(away_team.record)
+    home_ppg = home_team.pf / (home_team.wins + home_team.losses + home_team.ties)
+    away_ppg = away_team.pf / (away_team.wins + away_team.losses + away_team.ties)
 
-    home_score, away_score = random.normal(loc = home_ppg, scale = 18, size=1), random.normal(loc = away_ppg, scale = 18, size=1)
+    home_score = round(random.normal(loc = home_ppg, scale = 18, size=1)[0], 2)
+    away_score = round(random.normal(loc = away_ppg, scale = 18, size=1)[0], 2)
     
     home_team.pf+=home_score
-    home_team.pa+=away_score
     away_team.pf+=away_score
+
+    home_team.pa+=away_score
     away_team.pa+=home_score
 
     if override:
         if override == 1:
-            home_team.addWin()
-            away_team.addLoss()
+            home_team.wins+=1
+            away_team.losses+=1
         elif override == 2:
-            home_team.addLoss()
-            away_team.addWin()
+            home_team.losses+=1
+            away_team.wins+=1
+        elif override == 3:
+            home_team.ties+=1
+            away_team.ties+=1
         else:
             raise Exception("InvalidOverride")
     else:
         if home_score > away_score:
-            home_team.addWin()
-            away_team.addLoss()
+            home_team.wins+=1
+            away_team.losses+=1
+        elif home_score < away_score:
+            home_team.losses+=1
+            away_team.wins+=1
         else:
-            home_team.addLoss()
-            away_team.addWin()
+            home_team.ties+=1
+            away_team.ties+=1
 
 
 def printLeagueProbabilities(teams, sim_results, iterationsSoFar):
-    printf("| %25s | %4s | %4s | %8s | %8s |\n", "Team Name", "Wins", "Loss", "Playoff%","pf")
-    printf("-------------------------------------------------------------\n")
+    printf("| %25s | %4s | %4s | %4s | %8s | %8s |\n", "Team Name", "Wins", "Loss", "Ties", "Playoff%","pf")
+    printf("------------------------------------------------------------------------\n")
     for owner, team in list(teams.items()):
-        printf("| %25s | %4d | %4d | %8.1f | %8.2f |\n", team.name , team.getWins(), team.getLosses(), sim_results[owner]['appearances'] / float(iterationsSoFar)*100, sim_results[owner]['pf'])
+        printf("| %25s | %4.1f | %4.1f | %4.1f | %8.1f | %8.2f |\n", team.name, sim_results[owner]['wins'], sim_results[owner]['losses'], sim_results[owner]['ties'], sim_results[owner]['appearances'] / float(iterationsSoFar)*100, sim_results[owner]['pf'])
     printf("\n")
 
 def playoffs(teams, sim_results, iterations, slots = 4):
     # teamsSorted does not mutate the param teams
-    teamsSorted = sorted(teams.values(), key=operator.attrgetter('record','pf'), reverse=True)
+    teamsSorted = sorted(teams.values(), key=operator.attrgetter('wins','ties','losses','pf'), reverse=True)
     for i in range(0, slots):
         sim_results[teamsSorted[i].name]['appearances'] += 1
     for team in teams.values():
         sim_results[team.name]['pf'] += team.pf / iterations
+        sim_results[team.name]['wins'] += team.wins / iterations
+        sim_results[team.name]['losses'] += team.losses / iterations
+        sim_results[team.name]['ties'] += team.ties / iterations
 
 def main():
     print("Retrieving the Teams from ESPN Fantasy")
@@ -116,7 +115,7 @@ def main():
     
     sim_results = {}
     for team in teams.keys():
-        sim_results[team] = {'appearances': 0, 'pf': 0, 'wins': 0, 'losses': 0}
+        sim_results[team] = {'appearances': 0, 'pf': 0, 'wins': 0, 'losses': 0, 'ties': 0}
 
     iterations = 100
     printf("Running Sim with %d iterations...\n",iterations)
